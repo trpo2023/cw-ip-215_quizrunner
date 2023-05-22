044f5ffb7e29d1584a5924df1611ecf2d9a8f6a3
Maslakov Dmitriy <Hakiiiiii@github.com>
2023-05-22 22:39:06 +0700

    Added tests

---
+++ test-file/ctestlib.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctestlib.h>
+
+int main() {
+    int result1 = test_createTest(), result2 = test_editTest();
+	int result3 = TestplayQuiz("topictest1.txt");
+    if (result1) {
+        printf("\ntest_createTest completed!\n");
+    } else {
+        printf("\ntest_createTest failed!\n");
+    }
+    if (result2) {
+        printf("\ntest_editTest completed!\n");
+    } else {
+        printf("\ntest_editTest failed!\n");
+    }
+    if (result3) {
+        printf("\ntest_playQuiz completed!\n");
+    } else {
+        printf("\ntest_playQuiz failed!\n");
+    }
+	TestplayQuiz("topictest1.txt");
+	if(result1 + result2 + result3 == 3){
+		printf("\nAll tests passed successfully!\n");
+		printf("\n######     #     #####   #####\n#     #   # #   #     # #     #\n#     #  #   #  #       #      \n######  #     #  #####   ##### \n#       #######       #       #\n#       #     # #     # #     #\n#       #     #  #####   ##### \n");
+	}else{
+		printf("\n####### ######  ######  ####### ###### \n#       #     # #     # #     # #     #\n#       #     # #     # #     # #     #\n#####   ######  ######  #     # ###### \n#       #   #   #   #   #     # #   #  \n#       #    #  #    #  #     # #    # \n####### #     # #     # ####### #     #\n");
+	}
+    return 0;
+}
---
+++ test-file/ctestlib.h
@@ -0,0 +1,21 @@
+#ifndef CTESTLIB_H
+#define CTESTLIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int score;
+    char question[100];
+    char options[4][50];
+    int correctOption;
+} Question;
+
+int loadQuestions(const char *filename, Question *questions, int numQuestions);
+int TestplayQuiz(const char *filename);
+void createTest(const char* filename);
+int test_createTest();
+int test_editTest();
+
+#endif
