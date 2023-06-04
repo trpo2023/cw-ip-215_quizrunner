20eae313166ab17f2df63b638ba026925bbbd10f
DiSeDgE <124343022+DiSeDgE@users.noreply.github.com>
2023-06-04 23:30:39 +0700

    Update QuizRunner.c

--- src/project/QuizRunner.c
+++ src/project/QuizRunner.c
@@ -1,11 +1,11 @@
 int main() {
-  int count = 1;
-  while (count == 1) {
-    mainMenu();
-    printf("\n Do you want to go back to main menu?\n 1. Yes\n 2. Exit "
-           "game\n "
-           "Enter: ");
-    scanf("%d", &count);
-    printf("\n\n\n\n\n\n\n\n");
-  }
-  return 0;
+    int count = 1;
+    while (count == 1) {
+        mainMenu();
+        printf("\n Do you want to go back to main menu?\n 1. Yes\n 2. Exit "
+               "game\n "
+               "Enter: ");
+        scanf("%d", &count);
+        printf("\n\n\n\n\n\n\n\n");
+    }
+    return 0;
