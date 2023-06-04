503803c92308e091e49822f6fe4c9ff421c647e2
DiSeDgE <124343022+DiSeDgE@users.noreply.github.com>
2023-06-04 21:55:16 +0700

    Update quiz.h

--- src/project_lib/quiz.h
+++ src/project_lib/quiz.h
@@ -6,16 +6,16 @@
 #include <string.h>
 
 typedef struct {
-    char question[256];
-    char options[4][256];
-    int correctOption;
-    int score;
+  char question[256];
+  char options[4][256];
+  int correctOption;
+  int score;
 } Question;
 
-int loadQuestions(const char* filename, Question* questions, int numQuestions);
-void playQuiz(const char* filename);
-void createTest(const char* filename);
-void editTest(const char* filename);
+int loadQuestions(const char *filename, Question *questions, int numQuestions);
+void playQuiz(const char *filename);
+void createTest(const char *filename);
+void editTest(const char *filename);
 int mainMenu();
 
 #endif
