6fb2bb8b6fb5e7e7b390dfacae50367131ed9fd0
DiSeDgE <124343022+DiSeDgE@users.noreply.github.com>
2023-06-04 23:09:12 +0700

    Update CMakeCCompilerId.c

--- build/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c
+++ build/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c
@@ -664,17 +664,17 @@ char const* info_cray = "INFO"
 
 /* Convert integer to decimal digit literals.  */
 #define DEC(n)                                                           \
-	('0' + (((n) / 10000000) % 10)), ('0' + (((n) / 1000000) % 10)),     \
-	('0' + (((n) / 100000) % 10)), ('0' + (((n) / 10000) % 10)), \
-	('0' + (((n) / 1000) % 10)), ('0' + (((n) / 100) % 10)),     \
-	('0' + (((n) / 10) % 10)), ('0' + ((n) % 10))
+    ('0' + (((n) / 10000000) % 10)), ('0' + (((n) / 1000000) % 10)),     \
+    ('0' + (((n) / 100000) % 10)), ('0' + (((n) / 10000) % 10)), \
+    ('0' + (((n) / 1000) % 10)), ('0' + (((n) / 100) % 10)),     \
+    ('0' + (((n) / 10) % 10)), ('0' + ((n) % 10))
 
 /* Convert integer to hex digit literals.  */
 #define HEX(n)                                                    \
-	('0' + ((n) >> 28 & 0xF)), ('0' + ((n) >> 24 & 0xF)),         \
-	('0' + ((n) >> 20 & 0xF)), ('0' + ((n) >> 16 & 0xF)), \
-	('0' + ((n) >> 12 & 0xF)), ('0' + ((n) >> 8 & 0xF)),  \
-	('0' + ((n) >> 4 & 0xF)), ('0' + ((n)&0xF))
+    ('0' + ((n) >> 28 & 0xF)), ('0' + ((n) >> 24 & 0xF)),         \
+    ('0' + ((n) >> 20 & 0xF)), ('0' + ((n) >> 16 & 0xF)), \
+    ('0' + ((n) >> 12 & 0xF)), ('0' + ((n) >> 8 & 0xF)),  \
+    ('0' + ((n) >> 4 & 0xF)), ('0' + ((n)&0xF))
 
 /* Construct a string literal encoding the version number. */
 #ifdef COMPILER_VERSION
@@ -840,28 +840,28 @@ char* argv[];
 int main(int argc, char* argv[])
 #endif
 {
-	int require = 0;
-	require += info_compiler[argc];
-	require += info_platform[argc];
-	require += info_arch[argc];
+    int require = 0;
+    require += info_compiler[argc];
+    require += info_platform[argc];
+    require += info_arch[argc];
 #ifdef COMPILER_VERSION_MAJOR
-	require += info_version[argc];
+    require += info_version[argc];
 #endif
 #ifdef COMPILER_VERSION_INTERNAL
-	require += info_version_internal[argc];
+    require += info_version_internal[argc];
 #endif
 #ifdef SIMULATE_ID
-	require += info_simulate[argc];
+    require += info_simulate[argc];
 #endif
 #ifdef SIMULATE_VERSION_MAJOR
-	require += info_simulate_version[argc];
+    require += info_simulate_version[argc];
 #endif
 #if defined(__CRAYXT_COMPUTE_LINUX_TARGET)
-	require += info_cray[argc];
+    require += info_cray[argc];
 #endif
-	require += info_language_standard_default[argc];
-	require += info_language_extensions_default[argc];
-	(void)argv;
-	return require;
+    require += info_language_standard_default[argc];
+    require += info_language_extensions_default[argc];
+    (void)argv;
+    return require;
 }
 #endif
